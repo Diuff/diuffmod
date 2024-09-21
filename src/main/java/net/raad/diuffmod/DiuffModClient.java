d86e9c252675773c4f46671b69b68fb8fd56a879
Diuff <thiago.phillipe14@yahoo.com>
2024-09-21 13:53:36 -0300

    a

---
+++ src/main/java/net/raad/diuffmod/DiuffModClient.java
@@ -0,0 +1,10 @@
+package net.raad.diuffmod;
+
+import net.fabricmc.api.ClientModInitializer;
+public class DiuffModClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/raad/diuffmod/DiuffModDataGenerator.java
@@ -0,0 +1,11 @@
+package net.raad.diuffmod;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class DiuffModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
